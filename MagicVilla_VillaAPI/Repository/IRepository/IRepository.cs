f091620f6383e43d1531e8930090237af492e02c
Jitendra Patel <Jitendra.Patel@ey.com>
2023-10-16 12:09:22 +0530

    No chnage just testing

--- MagicVilla_VillaAPI/Repository/IRepository/IRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/IRepository.cs
@@ -6,7 +6,7 @@ namespace MagicVilla_VillaAPI.Repository.IRepository
     public interface IRepository<T> where T : class
      {
 
-        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);  
         Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
 
         Task CreateAsync(T entity);
